31b9b489bb4aa813e6e3be3b078e371524124630
Jacob Wallin <jacobwallin@gmail.com>
2021-02-13 14:03:02 -0600

    make transactions page private route

--- src/components/Views/Transactions.tsx
+++ src/components/Views/Transactions.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import PrivateRoute from "../Protected_Routes/PrivateRoute";
+import { useRouteMatch } from "react-router-dom";
 import AddCardsForm from "../AddCardsForm";
 
 export default function Transactions() {
-  return <AddCardsForm />;
+  const { path } = useRouteMatch();
+  return <PrivateRoute exact path={path} component={AddCardsForm} />;
 }
