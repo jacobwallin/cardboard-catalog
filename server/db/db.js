a496ac547f7171a766b01b62e954e5da5e359c80
Jacob Wallin <jacobwallin@gmail.com>
2021-05-15 23:32:31 -0500

    allow SSL support Sequelize

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,6 @@ const Sequelize = require("sequelize");
 
 const dbURL = process.env.DATABASE_URL || "postgres://localhost:5432/cards";
 
-const db = new Sequelize(dbURL);
+const db = new Sequelize(dbURL, { native: true });
 
 module.exports = db;
