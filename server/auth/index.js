02ab9c1eb164e143cd4485951c43eeecf66e7aea
Jacob Wallin <jacobwallin@gmail.com>
2021-09-13 05:14:09 -0500

    registration route doesn't log user in automatically after registration

--- server/auth/index.js
+++ server/auth/index.js
@@ -12,29 +12,23 @@ router.post("/login", passport.authenticate("local"), (req, res, next) => {
   res.json(req.user);
 });
 
-router.post(
-  "/register",
-  (req, res, next) => {
-    // explicitly destructure each field to prevent isAdmin or any other data from being sent to db
-    const { firstName, lastName, username, email, password } = req.body;
-    User.create({
-      firstName,
-      lastName,
-      username,
-      email,
-      password,
+router.post("/register", (req, res, next) => {
+  // explicitly destructure each field to prevent isAdmin or any other data from being sent to db
+  const { firstName, lastName, username, email, password } = req.body;
+  User.create({
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+  })
+    .then((user) => {
+      res.json(user);
     })
-      .then((user) => {
-        // At this point, user is not logged in and needs to be redirected to login page
-        next();
-      })
-      .catch((err) => res.send(err));
-  },
-  passport.authenticate("local"),
-  (req, res, next) => {
-    res.send("user has been registered and maybe logged in????");
-  }
-);
+    .catch((err) => {
+      next(err);
+    });
+});
 
 router.post("/logout", (req, res, next) => {
   req.logout();
