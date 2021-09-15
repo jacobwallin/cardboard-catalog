8b03334ce78838230d7408f57ab8aa557f162dc6
Jacob Wallin <jacobwallin@gmail.com>
2021-09-15 16:44:39 -0500

    login page shown by default instead of registration when user not logged in

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
 export default function Login() {
-  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(true);
   function toggle() {
     setShowLoginForm(!showLoginForm);
   }
