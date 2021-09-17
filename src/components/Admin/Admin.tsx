8510475dd803fc198b3f24c9560ac71ca2e0f827
Jacob Wallin <jacobwallin@gmail.com>
2021-09-16 20:05:40 -0500

    remove admin routes where unnecessary, and styled.ts

---
+++ src/components/Admin/Admin.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Route, useRouteMatch } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import AdminSets from "./all-sets/AdminSets";
+import EditSet from "./EditSet";
+import EditSubset from "./EditSubset";
+import EditSeries from "./EditSeries";
+import EditCard from "./EditCard";
+import * as Styled from "./styled";
+
+export default function Admin() {
+  const { path } = useRouteMatch();
+
+  return (
+    <Styled.AdminWrapper>
+      <Sidebar />
+      <Styled.AdminWrapper>
+        <Styled.AdminContainer>
+          <Route exact path={path} component={AdminSets} />
+          <Route exact path={`${path}/edit/set/:setId`} component={EditSet} />
+          <Route
+            exact
+            path={`${path}/edit/subset/:subsetId`}
+            component={EditSubset}
+          />
+          <Route
+            exact
+            path={`${path}/edit/series/:seriesId`}
+            component={EditSeries}
+          />
+          <Route
+            exact
+            path={`${path}/edit/card/:cardId`}
+            component={EditCard}
+          />
+          <Route exact path={`${path}/teams`} component={() => <p>teams</p>} />
+          <Route
+            exact
+            path={`${path}/attributes`}
+            component={() => <p>attributes</p>}
+          />
+          <Route
+            exact
+            path={`${path}/brands`}
+            component={() => <p>brands</p>}
+          />
+        </Styled.AdminContainer>
+      </Styled.AdminWrapper>
+    </Styled.AdminWrapper>
+  );
+}
---
+++ src/components/Admin/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AdminWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+export const AdminContainer = styled.div`
+  width: 1000px;
+`;
