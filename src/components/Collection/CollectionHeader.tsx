ffd59e2e422357c8626bde48a9f300e051176764
Jacob Wallin <jacobwallin@gmail.com>
2021-10-05 02:39:02 -0500

    add filter/browse toggle to collection header

--- src/components/Collection/CollectionHeader.tsx
+++ src/components/Collection/CollectionHeader.tsx
@@ -1,16 +1,68 @@
 import styled from "styled-components";
 import React from "react";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 25px;
+  margin-top: 10px;
+`;
 
 const StyledHeader = styled.div`
-  align-self: flex-start;
   font-size: 1.7em;
   font-weight: 600;
-  margin: 10px 0 25px 10px;
+  margin-left: 10px;
   color: #5a9bfd;
 `;
 
+const FilterBrowse = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledLink = styled(Link)<{ location: "LEFT" | "RIGHT" }>`
+  width: 80px;
+  height: 25px;
+  line-height: 25px;
+  text-align: center;
+  text-decoration: none;
+  color: white;
+  background-color: #5a9bfd;
+  &:hover {
+    background-color: #4182e4;
+  }
+  &:active {
+    background-color: #0e4fb1;
+  }
+  &:visited {
+    color: white;
+  }
+  border-left: ${(props) => props.location === "RIGHT" && "1px solid grey"};
+  border-top-left-radius: ${(props) => props.location === "LEFT" && "13px"};
+  border-bottom-left-radius: ${(props) => props.location === "LEFT" && "13px"};
+  border-top-right-radius: ${(props) => props.location === "RIGHT" && "13px"};
+  border-bottom-right-radius: ${(props) =>
+    props.location === "RIGHT" && "13px"};
+`;
+
 function CollectionHeader() {
-  return <StyledHeader>Your Collection</StyledHeader>;
+  return (
+    <Container>
+      <StyledHeader>Your Collection</StyledHeader>
+      <FilterBrowse>
+        <StyledLink to="/collection/filter" location="LEFT">
+          Filter
+        </StyledLink>
+        <StyledLink to="/collection" location="RIGHT">
+          Browse
+        </StyledLink>
+      </FilterBrowse>
+    </Container>
+  );
 }
 
 export default CollectionHeader;
