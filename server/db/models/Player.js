55ce4261cc63fbc748d85077fe14744e8ad14205
Jacob Wallin <jacobwallin@gmail.com>
2021-10-20 16:20:49 -0500

    add name birthday index to players to prevent duplicates

--- server/db/models/Player.js
+++ server/db/models/Player.js
@@ -1,26 +1,37 @@
 const db = require("../db");
 const { DataTypes } = require("sequelize");
 
-const Player = db.define("player", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Player = db.define(
+  "player",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthday: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    hallOfFame: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+    },
   },
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  birthday: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-  },
-  hallOfFame: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-  },
-  url: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ["name", "birthday"],
+      },
+    ],
+  }
+);
 
 module.exports = Player;
