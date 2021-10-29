70a76869cc9454789d9a065a34da111a01f72d30
Jacob Wallin <jacobwallin@gmail.com>
2021-10-29 00:19:02 -0500

    remove old function

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,20 +1,3 @@
-export function postData(url: string, payload: object) {
-  return fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(payload), // body data type must match "Content-Type" header
-  }).then((response) => {
-    return response.json();
-  });
-}
-
 export function get(url: string) {
   return fetch(url).then((response) => {
     // if response is not ok, throw it
