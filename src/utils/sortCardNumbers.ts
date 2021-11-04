d76714cc6d5a5e6130018a2f4ae39687c79e1334
Jacob Wallin <jacobwallin@gmail.com>
2021-11-04 00:17:00 -0500

    fix sorting card numbers with numbers and letters

--- src/utils/sortCardNumbers.ts
+++ src/utils/sortCardNumbers.ts
@@ -4,11 +4,20 @@ export default function (cardNumA: string, cardNumB: string): number {
     return +cardNumA - +cardNumB;
   }
 
-  // check if card number ends in number(s) and sort numerically
-  const matchesA = cardNumA.match(/\d+$/);
-  const matchesB = cardNumB.match(/\d+$/);
+  // check if card number ends or starts in number(s) and sort numerically
+  const matchesA = cardNumA.match(/^\d+/) || cardNumA.match(/\d+$/);
+  const matchesB = cardNumB.match(/^\d+/) || cardNumB.match(/\d+$/);
+  console.log("SORTING: ", matchesA, matchesB);
   if (matchesA && matchesB) {
-    return +matchesA[0] - +matchesB[0];
+    if (matchesA[0] !== matchesB[0]) {
+      return +matchesA[0] - +matchesB[0];
+    } else {
+      // if extracted numbers are equal, get only the string portion and compare that
+      let aRegex = new RegExp(matchesA[0]);
+      let bRegex = new RegExp(matchesB[0]);
+      cardNumA = cardNumA.replace(aRegex, "");
+      cardNumB = cardNumB.replace(bRegex, "");
+    }
   }
 
   // if number is not a number or ending in number(s), compare as strings
