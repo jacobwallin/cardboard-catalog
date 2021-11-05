60e43666f14f38222fdcbf212ca9c116320af016
Jacob Wallin <jacobwallin@gmail.com>
2021-11-04 20:32:49 -0500

    set default year sort

--- src/components/Collection/select_year/aggregateCards.ts
+++ src/components/Collection/select_year/aggregateCards.ts
@@ -6,31 +6,34 @@ export function aggregateCardsByYear(cardsBySet: SetCards[]): Array<{
   distinctCards: number;
   totalCards: number;
 }> {
-  return cardsBySet.reduce(
-    (
-      cardsByYear: Array<{
-        year: number;
-        distinctCards: number;
-        totalCards: number;
-      }>,
-      set
-    ) => {
-      if (
-        cardsByYear.length > 0 &&
-        cardsByYear[cardsByYear.length - 1].year ===
-          +set.release_date.slice(0, 4)
-      ) {
-        cardsByYear[cardsByYear.length - 1].distinctCards += +set.distinctCards;
-        cardsByYear[cardsByYear.length - 1].totalCards += +set.totalCards;
-      } else {
-        cardsByYear.push({
-          year: +set.release_date.slice(0, 4),
-          distinctCards: +set.distinctCards,
-          totalCards: +set.totalCards,
-        });
-      }
-      return cardsByYear;
-    },
-    []
-  );
+  return cardsBySet
+    .reduce(
+      (
+        cardsByYear: Array<{
+          year: number;
+          distinctCards: number;
+          totalCards: number;
+        }>,
+        set
+      ) => {
+        if (
+          cardsByYear.length > 0 &&
+          cardsByYear[cardsByYear.length - 1].year ===
+            +set.release_date.slice(0, 4)
+        ) {
+          cardsByYear[cardsByYear.length - 1].distinctCards +=
+            +set.distinctCards;
+          cardsByYear[cardsByYear.length - 1].totalCards += +set.totalCards;
+        } else {
+          cardsByYear.push({
+            year: +set.release_date.slice(0, 4),
+            distinctCards: +set.distinctCards,
+            totalCards: +set.totalCards,
+          });
+        }
+        return cardsByYear;
+      },
+      []
+    )
+    .sort((yearA, yearB) => yearB.year - yearA.year);
 }
