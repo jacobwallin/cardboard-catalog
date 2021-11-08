5653655f640d1b30920a3be5804a994ac72b76f5
Jacob Wallin <jacobwallin@gmail.com>
2021-11-08 03:15:33 -0600

    add year and complete columns, remove old db credentials

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const dbURL =
-  "postgres://bqqobezwecpmhg:6c7526a59c10d1efb71086e51f93a8428b4d6dcaddc7cca729502a69817f0306@ec2-3-217-91-165.compute-1.amazonaws.com:5432/d30vi3b56km00f";
+  process.env.HEROKU_POSTGRESQL_MAUVE_URL || "postgres://localhost:5432/cards";
 
 const db = new Sequelize(dbURL, { native: true });
 
