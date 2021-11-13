8550610b9f0c9f44ae34e546e9ef7f07836149d2
Jacob Wallin <jacobwallin@gmail.com>
2021-11-13 01:13:52 -0600

    handle player urls with periods in name

--- server/api/players/routes.js
+++ server/api/players/routes.js
@@ -71,7 +71,7 @@ router.post("/scrape", isAdmin, async (req, res, next) => {
 
   // validate url
   const valid =
-    /^https?:\/\/www.baseball-reference.com\/players\/[a-z]\/\w{4,7}\d{2}.shtml/.test(
+    /^https?:\/\/www.baseball-reference.com\/players\/[a-z.]\/[\w.]{4,7}\d{2}.shtml/.test(
       url
     ) ||
     /^https?:\/\/www.baseball-reference.com\/register\/player.fcgi\?id=/.test(
