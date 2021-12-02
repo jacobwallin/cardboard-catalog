0c305db06fa5a8d91dc1925bbe82174bf5556456
Jacob Wallin <jacobwallin@gmail.com>
2021-12-02 17:41:32 -0600

    update redux

--- src/store/library/teams/actions.ts
+++ src/store/library/teams/actions.ts
@@ -1,19 +1,56 @@
-import {
-  GET_ALL_TEAMS_REQUEST,
-  GET_ALL_TEAMS_SUCCESS,
-  GET_ALL_TEAMS_FAILURE,
-  TeamsActionTypes,
-  TeamsState,
-} from "./types";
+import * as types from "./types";
 
-export const getAllTeamsRequest = (): TeamsActionTypes => ({
-  type: GET_ALL_TEAMS_REQUEST,
+// get all teams
+export const getAllTeamsRequest = (): types.TeamsActionTypes => ({
+  type: types.GET_ALL_TEAMS_REQUEST,
 });
 
-export const getAllTeamsSuccess = (allTeams: TeamsState): TeamsActionTypes => ({
-  type: GET_ALL_TEAMS_SUCCESS,
+export const getAllTeamsSuccess = (
+  allTeams: types.TeamsState
+): types.TeamsActionTypes => ({
+  type: types.GET_ALL_TEAMS_SUCCESS,
   allTeams,
 });
-export const getAllTeamsFailure = (): TeamsActionTypes => ({
-  type: GET_ALL_TEAMS_FAILURE,
+export const getAllTeamsFailure = (): types.TeamsActionTypes => ({
+  type: types.GET_ALL_TEAMS_FAILURE,
+});
+
+// get teams by league
+export const getTeamsByLeagueRequest = (): types.TeamsActionTypes => ({
+  type: types.GET_TEAMS_BY_LEAGUE_REQUEST,
+});
+export const getTeamsByLeagueSuccess = (
+  teams: types.Team[]
+): types.TeamsActionTypes => ({
+  type: types.GET_TEAMS_BY_LEAGUE_SUCCESS,
+  teams,
+});
+export const getTeamsByLeagueFailure = (): types.TeamsActionTypes => ({
+  type: types.GET_TEAMS_BY_LEAGUE_FAILURE,
+});
+
+// add team
+export const addTeamRequest = (): types.TeamsActionTypes => ({
+  type: types.ADD_TEAM_REQUEST,
+});
+export const addTeamSuccess = (team: types.Team): types.TeamsActionTypes => ({
+  type: types.ADD_TEAM_SUCCESS,
+  team,
+});
+export const addTeamFailure = (): types.TeamsActionTypes => ({
+  type: types.ADD_TEAM_FAILURE,
+});
+
+// update team
+export const updateTeamRequest = (): types.TeamsActionTypes => ({
+  type: types.UPDATE_TEAM_REQUEST,
+});
+export const updateTeamSuccess = (
+  team: types.Team
+): types.TeamsActionTypes => ({
+  type: types.UPDATE_TEAM_SUCCESS,
+  team,
+});
+export const updateTeamFailure = (): types.TeamsActionTypes => ({
+  type: types.UPDATE_TEAM_FAILURE,
 });
