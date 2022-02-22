2e80d6b648f163b6ae095c3f48dd2cec6924f447
Jacob Wallin <jacobwallin@gmail.com>
2022-02-22 16:29:24 -0600

    connect transactions reducer

--- src/store/collection/index.ts
+++ src/store/collection/index.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import browseCollectionReducer from "./browse/reducer";
 import filterCollectionReducer from "./filter/reducer";
+import transactionsReducer from "./transactions/reducer";
 
 const collectionReducer = combineReducers({
   browse: browseCollectionReducer,
   filter: filterCollectionReducer,
+  transactions: transactionsReducer,
 });
 
 export default collectionReducer;
