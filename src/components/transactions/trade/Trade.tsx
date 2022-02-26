a2632125581ebdd813eecf58af275d18e376798d
Jacob Wallin <jacobwallin@gmail.com>
2022-02-26 16:42:52 -0600

    remove switch

--- src/components/transactions/trade/Trade.tsx
+++ src/components/transactions/trade/Trade.tsx
@@ -3,6 +3,7 @@ import SelectTraded from "./select-traded/SelectTraded";
 import SelectReceived from "./select-received/SelectReceived";
 import ConfirmTrade from "./confirm/ConfirmTrade";
 import { CardFormData } from "../quick-add/AddCardsForm";
+import { CardData } from "../../../store/collection/browse/types";
 type StepNumbers = 1 | 2 | 3;
 
 export default function Trade() {
@@ -20,22 +21,37 @@ export default function Trade() {
     setReceivedCards(receivedCards);
   }
 
-  switch (currentStep) {
-    case 1:
-      return (
-        <SelectTraded
-          tradedCards={tradedCards}
-          handleTradedCardsChange={handleTradedCardsChange}
-        />
-      );
-    case 2:
-      return (
-        <SelectReceived
-          receivedCards={receivedCards}
-          handleReceivedCardsChange={handleReceivedCardsChange}
-        />
-      );
-    case 3:
-      return <ConfirmTrade />;
+  function submitTradedCards(cardData: CardData[]) {
+    // save card data
+    //
   }
+  function submitReceivedCards(cardData: CardData[]) {}
+
+  return (
+    <>
+      <div>Enter A Trade</div>
+
+      {currentStep === 1 && (
+        <>
+          <SelectTraded
+            tradedCards={tradedCards}
+            handleTradedCardsChange={handleTradedCardsChange}
+          />
+        </>
+      )}
+      {currentStep === 2 && (
+        <>
+          <SelectReceived
+            receivedCards={receivedCards}
+            handleReceivedCardsChange={handleReceivedCardsChange}
+          />
+        </>
+      )}
+      {currentStep === 3 && (
+        <>
+          <ConfirmTrade />
+        </>
+      )}
+    </>
+  );
 }
