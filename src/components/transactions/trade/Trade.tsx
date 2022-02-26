b8e8cfbd58b3807d00f3ae4f449b6be5fe1b0cee
Jacob Wallin <jacobwallin@gmail.com>
2022-02-26 02:42:29 -0600

    outline

--- src/components/transactions/trade/Trade.tsx
+++ src/components/transactions/trade/Trade.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+type StepNumbers = 1 | 2 | 3;
 
 export default function Trade() {
+  // track current step of trade data entry use is on
+  const [currentStep, setCurrentStep] = useState<StepNumbers>(1);
+
+  // step 1: enter cards traded
+
+  // step 2: enter cards received
+
+  // step 3: enter trade details and confirm transaction
+
   return <div>Do we have a deal?</div>;
 }
