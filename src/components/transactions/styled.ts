25444111ff44ba85829de06ebc3586deca608607
Jacob Wallin <jacobwallin@gmail.com>
2022-03-12 02:48:49 -0600

    styling

--- src/components/transactions/styled.ts
+++ src/components/transactions/styled.ts
@@ -3,25 +3,33 @@ import { Link } from "react-router-dom";
 
 export const TransactionLink = styled(Link)`
   width: 100px;
-  height: 75px;
+  height: 45px;
   border: 1px solid gray;
   border-radius: 5px;
-
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  font-size: 0.9rem;
   &:hover {
-    color: red;
+    font-weight: 500;
+    border: 1px solid black;
   }
 `;
 
 export const Header = styled.div`
-  font-size: 1.5em;
+  font-size: 1.2rem;
   font-weight: 400;
   margin-left: 10px;
   margin-bottom: 20px;
+  align-self: flex-start;
 `;
 
 export const TransactionsContainer = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 25px;
-  margin-bottom: 50px;
+  gap: 20px;
+  margin: 0 0 50px 25px;
+  align-self: flex-start;
 `;
