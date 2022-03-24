61ee7520ec6c3d906f0f07397b9e009894790e45
Jacob Wallin <jacobwallin@gmail.com>
2022-03-24 16:19:17 -0500

    sort subsets by attributes

--- src/components/Admin/set/AdminSet.tsx
+++ src/components/Admin/set/AdminSet.tsx
@@ -15,22 +15,7 @@ import { LoadingDots } from "../../shared/Loading";
 import { DataTableWrapper } from "../components/WrappedDataTable";
 import DataTable from "react-data-table-component";
 import dataTableColumns from "./dataTableColumns";
-
-const columns = [
-  {
-    name: "Name",
-    selector: (row: any) =>
-      row.prefix !== "" ? `${row.name} (${row.prefix})` : `${row.name}`,
-    sortable: true,
-    grow: 1,
-  },
-  {
-    name: "",
-    sortable: false,
-    cell: (row: any) => <EditLink to={`/admin/subset/${row.id}`} />,
-    grow: 0,
-  },
-];
+import aggregateSubsets, { AggregatedSubsets } from "./aggregateSubsets";
 
 const isLoadingSelector = createLoadingSelector(["GET_SINGLE_SET"]);
 const creatingSubsetSelector = createLoadingSelector(["CREATE_SUBSET"]);
@@ -43,6 +28,9 @@ export default function SetAdminPage() {
     creatingSubsetSelector(state)
   );
   const set = useSelector((state: RootState) => state.library.sets.set);
+  const [aggregatedSubsets, setAggregatedSubsets] = useState<
+    AggregatedSubsets | undefined
+  >(undefined);
 
   useEffect(() => {
     if (setId) dispatch(fetchSet(+setId));
@@ -55,15 +43,19 @@ export default function SetAdminPage() {
     }
   }, [creatingSubset]);
 
+  useEffect(() => {
+    if (set.subsets.length > 0) {
+      setAggregatedSubsets(
+        aggregateSubsets(set.subsets, set.baseSubsetId || 0)
+      );
+    }
+  }, [set]);
+
   const [showCreateSubsetModal, setShowCreateSubsetModal] = useState(false);
 
   function toggleCreateSubsetModal() {
     setShowCreateSubsetModal(!showCreateSubsetModal);
   }
-
-  const baseSubset = set.subsets.find((subset) => {
-    return subset.id === set.baseSubsetId;
-  });
 
   if (isLoading || !setId || set.id !== +setId) {
     return (
@@ -77,35 +69,54 @@ export default function SetAdminPage() {
     <AdminPageContainer>
       <EditFormHeader text={`${set.name}`} />
       <EditSet setId={+setId} />
-      <DataTableWrapper>
-        <DataTable
-          dense
-          highlightOnHover
-          title={`Base Set`}
-          columns={dataTableColumns}
-          data={baseSubset ? [baseSubset] : []}
-        />
-      </DataTableWrapper>
-      <WrappedDataTable
-        dense
-        title={`Inserts and Other Sets`}
-        columns={dataTableColumns}
-        data={set.subsets
-          .filter((subset) => {
-            return subset.id !== set.baseSubsetId;
-          })
-          .sort((a, b) => {
-            if (a.name < b.name) return -1;
-            if (a.name > b.name) return 1;
-            return 0;
-          })}
-        highlightOnHover
-        actions={
-          <CreateButton onClick={toggleCreateSubsetModal}>
-            Create Subset
-          </CreateButton>
-        }
-      />
+      {aggregatedSubsets && (
+        <>
+          <DataTableWrapper>
+            <DataTable
+              dense
+              highlightOnHover
+              title={`Base Set`}
+              columns={dataTableColumns}
+              data={aggregatedSubsets.base ? [aggregatedSubsets?.base] : []}
+              actions={
+                <CreateButton onClick={toggleCreateSubsetModal}>
+                  Create Subset
+                </CreateButton>
+              }
+            />
+          </DataTableWrapper>
+          <WrappedDataTable
+            dense
+            title={`Short Print Sets`}
+            columns={dataTableColumns}
+            data={aggregatedSubsets.shortPrints}
+            pagination
+            paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
+            paginationPerPage={5}
+            highlightOnHover
+          />
+          <WrappedDataTable
+            dense
+            title={`Insert Sets`}
+            columns={dataTableColumns}
+            data={aggregatedSubsets.inserts}
+            pagination
+            paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
+            paginationPerPage={10}
+            highlightOnHover
+          />
+          <WrappedDataTable
+            dense
+            title={`Auto & Relic Sets`}
+            columns={dataTableColumns}
+            data={aggregatedSubsets.autoRelic}
+            pagination
+            paginationRowsPerPageOptions={[5, 10, 15, 20, 25, 30]}
+            paginationPerPage={10}
+            highlightOnHover
+          />
+        </>
+      )}
       {showCreateSubsetModal && (
         <CreateSubsetModal
           handleCancel={toggleCreateSubsetModal}
