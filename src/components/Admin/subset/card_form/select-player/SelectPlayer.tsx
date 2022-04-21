1927bef62dddb677f1d928641c4115aa9d51764e
Jacob Wallin <jacobwallin@gmail.com>
2022-04-20 19:50:10 -0500

    move data table properties to module, shrink row height

--- src/components/Admin/subset/card_form/select-player/SelectPlayer.tsx
+++ src/components/Admin/subset/card_form/select-player/SelectPlayer.tsx
@@ -3,42 +3,8 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../../../store";
 import { Player } from "../../../../../store/library/players/types";
 import DataTable from "react-data-table-component";
-import LinkIconWrapper from "../shared/Link";
-import { ReactComponent as LinkIcon } from "../shared/link.svg";
 import * as Styled from "./styled";
-import StyledButton from "../../../components/StyledButton";
-
-const columns = (addPlayer: (player: Player) => void) => [
-  { name: "Name", selector: (row: Player) => row.name, sortable: true },
-  { name: "DOB", selector: (row: Player) => row.birthday, sortable: true },
-  {
-    name: "Page",
-    cell: (row: Player) => (
-      <LinkIconWrapper href={row.url} target="_blank" rel="noopener noreferrer">
-        <LinkIcon />
-      </LinkIconWrapper>
-    ),
-    sortable: true,
-    grow: 0,
-  },
-  {
-    name: "",
-    selector: (row: Player) => row.url,
-    cell: (row: Player) => (
-      <StyledButton
-        color="BLUE"
-        fontSize=".8rem"
-        width="50px"
-        height="20px"
-        onClick={() => addPlayer(row)}
-      >
-        Add
-      </StyledButton>
-    ),
-    sortable: true,
-    grow: 0,
-  },
-];
+import { columns, customStyles } from "./dataTable";
 
 interface Props {
   addPlayer(player: Player): void;
@@ -67,7 +33,9 @@ export default function SelectPlayer(props: Props) {
         pagination
         dense
         noHeader
-        paginationRowsPerPageOptions={[10]}
+        paginationRowsPerPageOptions={[10, 15]}
+        customStyles={customStyles}
+        highlightOnHover
       />
     </Styled.Container>
   );
---
+++ src/components/Admin/subset/card_form/select-player/dataTable.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import StyledButton from "../../../components/StyledButton";
+import { Player } from "../../../../../store/library/players/types";
+import LinkIconWrapper from "../shared/Link";
+import { ReactComponent as LinkIcon } from "../shared/link.svg";
+
+export const columns = (addPlayer: (player: Player) => void) => [
+  { name: "Name", selector: (row: Player) => row.name, sortable: true },
+  { name: "DOB", selector: (row: Player) => row.birthday, sortable: true },
+  {
+    name: "Page",
+    cell: (row: Player) => (
+      <LinkIconWrapper href={row.url} target="_blank" rel="noopener noreferrer">
+        <LinkIcon />
+      </LinkIconWrapper>
+    ),
+    sortable: true,
+    grow: 0,
+  },
+  {
+    name: "",
+    selector: (row: Player) => row.url,
+    cell: (row: Player) => (
+      <StyledButton
+        color="BLUE"
+        fontSize=".8rem"
+        width="50px"
+        height="20px"
+        onClick={() => addPlayer(row)}
+      >
+        Add
+      </StyledButton>
+    ),
+    sortable: true,
+    grow: 0,
+  },
+];
+
+export const customStyles = {
+  rows: {
+    denseStyle: {
+      minHeight: "28px",
+    },
+  },
+};
