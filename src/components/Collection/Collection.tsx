cc9bc64dbad28ece745c8c8c40ddf06e0b3491d6
Jacob Wallin <jacobwallin@gmail.com>
2022-04-21 03:23:41 -0500

    return to my collection button

--- src/components/Collection/Collection.tsx
+++ src/components/Collection/Collection.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
 import { Route, Routes } from "react-router-dom";
 import SelectYear from "./select_year/SelectYear";
 import SelectSet from "./select_set/SelectSet";
@@ -8,13 +10,35 @@ import FilterPage from "./filter/FilterPage";
 import CollectionWrapper from "../shared/CollectionWrapper";
 import CollectionContainer from "../shared/CollectionContainer";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
+import ReturnToMyCollection from "./shared/ReturnToMyCollection";
+import { viewMyCollection } from "../../store/collection/browse/actions";
 
 export default function Collection() {
+  const dispatch = useDispatch();
+  const collectionFriend = useSelector(
+    (state: RootState) => state.collection.browse.friend
+  );
+
+  function returnToMyCollection() {
+    dispatch(viewMyCollection());
+  }
+
   return (
     <CollectionWrapper>
       <CollectionContainer>
         <Breadcrumbs />
-        <PageHeader title="My Collection">
+        {collectionFriend.id !== 0 && (
+          <ReturnToMyCollection onClick={returnToMyCollection}>
+            Return to My Collection
+          </ReturnToMyCollection>
+        )}
+        <PageHeader
+          title={
+            collectionFriend.id !== 0
+              ? `${collectionFriend.username}'s Collection`
+              : "My Collection"
+          }
+        >
           <CollectionToggle />
         </PageHeader>
         <Routes>
