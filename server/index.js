ca545fd17a3e345254456b13c7289372276c8a6b
Jacob Wallin <jacobwallin@gmail.com>
2022-08-18 18:07:34 -0500

    remove that

--- server/index.js
+++ server/index.js
@@ -32,13 +32,6 @@ app.use(
   })
 );
 
-// test loading indicators client-side
-// app.use((req, res, next) => {
-//   setTimeout(() => {
-//     next();
-//   }, 500);
-// });
-
 app.use(passport.initialize());
 app.use(passport.session());
 
