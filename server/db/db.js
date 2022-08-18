1dbf1b76b647cac03ddefd5974091e820be59bfe
Jacob Wallin <jacobwallin@gmail.com>
2022-08-18 18:30:53 -0500

    will this do anything?

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,6 @@ const Sequelize = require("sequelize");
 const dbURL =
   process.env.HEROKU_POSTGRESQL_MAUVE_URL || "postgres://localhost:5432/cards";
 
-const db = new Sequelize(dbURL, { native: true });
+const db = new Sequelize(dbURL, { native: true, ssl: true });
 
 module.exports = db;
