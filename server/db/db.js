181708f15930e86ec53834d512054059fb21dc05
Jacob Wallin <jacobwallin@gmail.com>
2022-08-18 19:59:43 -0500

    ssl not supported for local server

--- server/db/db.js
+++ server/db/db.js
@@ -3,13 +3,15 @@ const Sequelize = require("sequelize");
 const dbURL =
   process.env.HEROKU_POSTGRESQL_MAUVE_URL || "postgres://localhost:5432/cards";
 
-const db = new Sequelize(dbURL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const db = process.env.HEROKU_POSTGRESQL_MAUVE_URL
+  ? new Sequelize(dbURL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(dbURL);
 
 module.exports = db;
